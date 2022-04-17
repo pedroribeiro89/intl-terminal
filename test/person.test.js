30a8b425de981ef99ebf136d64e432507b9f2a1b
pedroribeiro89 <pedroribeiro89@hotmail.com>
2022-04-17 10:12:58 -0300

    test descriptions

--- test/person.test.js
+++ test/person.test.js
@@ -1,1 +1,4 @@
+//should create a person from object
+//should return object with formatted data to a given language
 //should return a person instance from a string
+//should return an error if trying to create a person with a invalid string
---
+++ test/terminalController.test.js
@@ -0,0 +1,3 @@
+// should create terminal controller with data loaded from database, print and terminal
+// should close terminal
+// should update table
